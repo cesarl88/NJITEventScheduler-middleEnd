869b9d51d86b9558bac6484a13f9995b73c16008
sabes231 <sabes231@yahoo.com>
2016-04-03 14:17:55 -0400

    Mdified getUserName

--- getUserName.php
+++ getUserName.php
@@ -6,7 +6,7 @@
 	function getUserName($UserID){
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, "https://web.njit.edu/~cls33/CS490/getUserName.php"); 
-		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array( "UserID" 		=> $UserID);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array( "UserID" 	=> $UserID)));
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		$CreateEventResult = curl_exec($ch);
 		curl_close($ch);
