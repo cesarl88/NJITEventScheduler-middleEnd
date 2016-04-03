3a07435c9dcb4b10b6660c8657ea0bd4c1aa293e
sabes231 <sabes231@yahoo.com>
2016-04-03 18:48:01 -0400

    update AddToGoogleC and UpdateEvent

--- AddToGoogleC.php
+++ AddToGoogleC.php
@@ -1,31 +1,60 @@
 <?
-	ini_set('display_errors', 'On');
-	error_reporting(E_ALL);
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);	
 	
 	
-	function createEnvent($eventID){
-		Event event = new Event();
-		event.setId($eventID);
-		client.events().insert("primary", event).execute();
-		echo json_encode(array('Passed'=> "1"),JSON_PRETTY_PRINT)
+function addGoogleCalendarEvent($title, $Place, $description, $startDate, $EndDate){
+	$event = new Google_Service_Calendar_Event(
+		array(
+		  'summary' 		=> $title,
+		  'location' 		=> $Place,
+		  'description' 	=> $description,
+		  'start' => array(
+			 'dateTime' => $startDate,
+			 'timeZone' => 'America/New_York',
+		  ),
+		  'end' => array(
+			 'dateTime' => $EndDate,
+			 'timeZone' => 'America/New_York',
+		  ),
+		  'recurrence' => array(
+			 'RRULE:FREQ=DAILY;COUNT=2'
+		  ),
+		  'reminders' => array(
+				'useDefault' => FALSE,
+				'overrides' => array(
+						array('method' => 'email', 'minutes' => 24 * 60),
+			 ),
+		  ),
+	));
+	$calendarId = 'primary';
+	$event = $service->events->insert($calendarId, $event);
+	printf('Event created: %s\n', $event->htmlLink);
 	}
 	
-	
-
-
-
-	function CalendarListEntry($eventID){
-		$calendarListEntry = new Google_Service_Calendar_CalendarListEntry();
-		$calendarListEntry->setId($eventID);
-		$createdCalendarListEntry = $service->calendarList->insert($calendarListEntry);
-		echo $createdCalendarListEntry->getSummary();
-	}
+	$title			= $_POST['title'];
+	$Place			= $_POST['Place'];
+	$description	= $_POST['description'];
+	$startDate   	= $_POST['startDate'];
+	$EndDate     	= $_POST['EndDate'];
+	$startTime   	= $_POST['startTime'];
+	$endTime     	= $_POST['endTime'];
 	
 	
+	#Format Start date with form '2015-05-28T09:00:00-04:00'
+	#"Y-m-d\TH:i:sP"
 	
-	$eventID =  $_POST['eventID'];
-	$result1 =  createEnvent($eventID);
-	$result2 =  CalendarListEntry($eventID);
+	$startDate 	= $startDate.'T'.$startTime.'-04:00';
+	var_dump($startDate);
+	
+	
+	#Format End date with form '2015-05-28T17:00:00-04:00'
+	$EndDate 	= $EndDate.'T'.$endTime.'-04:00';
+	var_dump($EndDate);
+	
+	
+	
+	
 	
 	
 	
--- UpdateEvent.php
+++ UpdateEvent.php
@@ -3,61 +3,57 @@
 	#include_once('login.php');
 	
 	#Function Definition
-	function UpdateEvent($title,$startDate,$EndDate,$startTime,$endTime,$Place,$Submiter,$eventname,$Organization,$image,$link,$description,$Approved ,$UserID ){
+	function UpdateEvent($UserID,$ID,$title,$startDate,$EndDate,$startTime,$endTime,$Place,$Submitter,$Organization,$eventname,$image,$link,$description,$Approved ){
 		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, "https://web.njit.edu/~cls33/CS490/CreateEvent.php"); 
+		curl_setopt($ch, CURLOPT_URL, "https://web.njit.edu/~cls33/CS490/UpdateEvent.php"); 
 		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
-																						"UserID"	=>	$UserID,
-																						"ID"	=>	$ID,
-																						"title"	=>	$title,
-																						"startDate"	=>	$startDate,
-																						"EndDate"	=>	$EndDate,
-																						"startTime"	=>	$startTime,
-																						"endTime"	=>	$endTime,
-																						"Place"	=>	$Place,
-																						"Submitter"	=>	$Submitter,
+																						"UserID"			=>	$UserID,
+																						"ID"				=>	$ID,
+																						"title"			=>	$title,
+																						"startDate"		=>	$startDate,
+																						"EndDate"		=>	$EndDate,
+																						"startTime"		=>	$startTime,
+																						"endTime"		=>	$endTime,
+																						"Place"			=>	$Place,
+																						"Submitter"		=>	$Submitter,
 																						"Organization"	=>	$Organization,
-																						"eventname"	=>	$eventname,
-																						"image"	=>	$image,
-																						"link"	=>	$link,
+																						"eventname"		=>	$eventname,
+																						"image"			=>	$image,
+																						"link"			=>	$link,
 																						"description"	=>	$description,
-																						"Approved"	=>	$Approved,
-																						)
-																				)
-						);
-		
+																						"Approved"		=>	$Approved
+																						)));
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		$CreateEventResult = curl_exec($ch);
+		$UpdateEventResult = curl_exec($ch);
 		curl_close($ch);
 		//echo "<br/>";
 		#$value = (json_decode($CreateEventResult,true));
 		
 		#pending
-		return $CreateEventResult;	#review reply from DB
-		
+		return $UpdateEventResult;	#review reply from DB	
 	}
 	
 	
 	
 	#Post Variables
-	$ID				= $_POST['ID'];
-	$title			= $_POST['title'];
-	$startDate		= $_POST['startDate'];
-	$EndDate			= $_POST['EndDate'];
-	$startTime		= $_POST['startTime'];
-	$endTime			= $_POST['endTime'];
-	$Place			= $_POST['Place'];
-	$Submitter		= $_POST['Submitter'];
-	$eventname		= $_POST['eventname'];
-	$Organization	= $_POST['Organization'];
-	$image			= $_POST['image'];  
-	$link				= $_POST['link'];
-	$description	= $_POST['description']; 
+	#$ID				= $_POST['ID'];
+	#$title			= $_POST['title'];
+	#$startDate		= $_POST['startDate'];
+	#$EndDate			= $_POST['EndDate'];
+	#$startTime		= $_POST['startTime'];
+	#$endTime			= $_POST['endTime'];
+	#$Place			= $_POST['Place'];
+	#$Submitter		= $_POST['Submitter'];
+	#$eventname		= $_POST['eventname'];
+	#$Organization	= $_POST['Organization'];
+	#$image			= $_POST['image'];  
+	#$link				= $_POST['link'];
+	#$description	= $_POST['description']; 
 	
 	
 	#function
 	$UserID			= $_POST['UserID'];
-	$ID				= $_POST['ID'];
+	$ID				= $_POST['ID'];				#EventID
 	$title			= $_POST['title'];			#
 	$startDate		= $_POST['startDate'];		#
 	$EndDate			= $_POST['EndDate'];			#
@@ -79,7 +75,7 @@
 	#
 	
 	#Function call
-	$result = functionCall();
+	$result = UpdateEvent();
 	print_r(json_encode( $result ,true));
 
 ?>
