34dc588a9d64a0e7be0c632ca179152ed14d4c7f
sabes231 <sabes231@yahoo.com>
2016-04-04 06:06:28 -0400

    List and getEventWeek

--- ListscheduleEventByUserID.php
+++ ListscheduleEventByUserID.php
@@ -1,2 +1,55 @@
-usuario 
-mostrar eventos al que ha sido subscrided
+<?php
+	session_start();
+	#include_once('login.php');
+	include_once('getEventByID.php');
+	#Function Definition
+	
+	
+	function getEventByID($UserID){
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "https://web.njit.edu/~cls33/CS490/ListscheduleEventByUserID.php"); 
+		curl_setopt($ch, CURLOPT_POSTFIELDS, "UserID=".$UserID);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$getEventReply = curl_exec($ch);
+		curl_close($ch);
+		#echo "<br/>";
+		#$value = (json_decode($getEventReply,true));
+		
+		
+		return $getEventReply;	#review reply from DB	
+	}
+	
+	
+	
+	
+	if($schedule)
+  {
+  	$result = [];
+  	foreach($schedule as $item)
+  	{
+  		$result[] = $item->getJSON();
+  		   	/*	$test = json_decode($item->getJSON(),true);
+  		 echo 'Title : '.$test['Title'].'<br/>'; */
+  	}
+  	echo json_encode(array('Event' => $result));
+  }
+  else
+  {
+  	echo json_encode(array('Event' => -1));
+  }
+  
+  
+  
+  
+  
+	
+	#Function call
+	$result = functionCall();
+	print_r(json_encode( $result ,true));
+
+?>
+
+
+
+
+
