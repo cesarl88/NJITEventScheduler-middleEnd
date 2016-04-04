50ac759982433e95a8ef5336a2dc63438a57258b
sabes231 <sabes231@yahoo.com>
2016-04-04 11:17:53 -0400

    Approve

--- approveEvent.php
+++ approveEvent.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
-	#ini_set('display_errors', 'On');
-	#error_reporting(E_ALL);
+	ini_set('display_errors', 'On');
+	error_reporting(E_ALL);
 	
 	
 	#include_once('login.php');
@@ -21,24 +21,72 @@
 		
 		return $getEventReply;	#review reply from DB	
 	}
-
+	
+	
+	function UpdateEvent($UserID,$ID,$title,$startDate,$EndDate,$startTime,$endTime,$Place,$Submitter,$Organization,$eventname,$image,$link,$description){
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "https://web.njit.edu/~cls33/CS490/UpdateEvent.php"); 
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
+																						"UserID"				=>	$UserID,
+																						"ID"						=>	$ID,
+																						"title"					=>	$title,
+																						"startDate"			=>	$startDate,
+																						"EndDate"				=>	$EndDate,
+																						"startTime"			=>	$startTime,
+																						"endTime"				=>	$endTime,
+																						"Place"					=>	$Place,
+																						"Submitter"			=>	$Submitter,
+																						"Organization"	=>	$Organization,
+																						"eventname"			=>	$eventname,
+																						"image"					=>	$image,
+																						"link"					=>	$link,
+																						"description"		=>	$description,
+																						"Approved"			=>	1
+																						)));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$UpdateEventResult = curl_exec($ch);
+		curl_close($ch);
+		//echo "<br/>";
+		#$value = (json_decode($CreateEventResult,true));
+		
+		#pending
+		return $UpdateEventResult;	#review reply from DB	
+	}
+	
+	
 	
 	#getEventByID
 	$ID				= $_POST['ID'];
 	
 	#Function call
-	$result = getEventByID($ID);
+	$result 	= getEventByID($ID);
 	
 	
 	#print_r ($result);
-	$result = json_decode($result, true);
+	$result 	= json_decode($result, true);
 	$varArray = $result['Event'];
-	var_dump ($result);
-	var_dump ($varArray);
+	#var_dump ($result);
+	#var_dump ($varArray);
 	
 	
+	$UserID					= $varArray['UserID'];
+	$ID							= $varArray['ID'];
+	$title					= $varArray['Title'];
+	$startDate			= $varArray['startDate'];
+	$EndDate				= $varArray['EndDate'];
+	$startTime			= $varArray['startTime'];
+	$endTime				= $varArray['endTime'];
+	$Place					= $varArray['Place'];
+	$Submitter			= $varArray['Submitter'];
+	$Organization		= $varArray['Organization'];
+	$eventname			= $varArray['EventName'];
+	$image					= $varArray['Image'];
+	$link						= $varArray['link'];
+	$description    = $varArray['Description'];
 	
 	
+	$resultApprove = UpdateEvent($UserID,$ID,$title,$startDate,$EndDate,$startTime,$endTime,$Place,$Submitter,$Organization,$eventname,$image,$link,$description);
+	print_r($resultApprove);
 	
 	
 ?>
