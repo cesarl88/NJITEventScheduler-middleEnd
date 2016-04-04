504ffefb98960a52e88714385260f3c8d91e1efb
sabes231 <sabes231@yahoo.com>
2016-04-04 10:26:15 -0400

    approve

---
+++ approveEvent.php
@@ -0,0 +1,43 @@
+<?php
+	session_start();
+	#ini_set('display_errors', 'On');
+	#error_reporting(E_ALL);
+	
+	
+	#include_once('login.php');
+	
+	#Function Definition
+	# Get json with event information where ID 
+	function getEventByID($ID){
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "https://web.njit.edu/~cls33/CS490/getEventByID.php"); 
+		curl_setopt($ch, CURLOPT_POSTFIELDS, "ID=".$ID);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$getEventReply = curl_exec($ch);
+		curl_close($ch);
+		#echo "<br/>";
+		#$value = (json_decode($getEventReply,true));
+		
+		
+		return $getEventReply;	#review reply from DB	
+	}
+
+	
+	#getEventByID
+	$ID				= $_POST['ID'];
+	
+	#Function call
+	$result = getEventByID($ID);
+	
+	
+	print_r ($result);
+	
+	
+
+	
+	
+	
+	
+	
+	
+?>
