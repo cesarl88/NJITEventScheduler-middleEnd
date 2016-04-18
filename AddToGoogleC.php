286bbe72b8e1af181423c20d569129e6f95533f1
sabes231 <sabes231@yahoo.com>
2016-04-18 18:27:09 -0400

    Add GCal and quicks

--- AddToGoogleC.php
+++ AddToGoogleC.php
@@ -1,43 +1,59 @@
 <?php
-ini_set('display_errors', 'On');
-error_reporting(E_ALL);	
+	#ini_set('display_errors', 'On');
+	#error_reporting(E_ALL);	
 
-include_once('formatDate.php');
-require_once __DIR__ . '/vendor/autoload.php';
+	include_once('formatDate.php');
+	require_once __DIR__ . '/vendor/autoload.php';
+	define('APPLICATION_NAME', 'Google Calendar API PHP Quickstart');
+	define('CREDENTIALS_PATH', '~/.credentials/calendar-php-quickstart.json');
+	define('CLIENT_SECRET_PATH', __DIR__ . '/client_secret.json');
 
 
-function addGoogleCalendarEvent($title, $Place, $description, $startD, $EndD, $linkSite){
-	$event = new Google_Service_Calendar_Event(
-		array(
-		  'summary' 		=> $title,
-		  'location' 		=> $Place,
-		  'description' 	=> $description,
-		  'htmlLink'		=> $linkSite,
-		  'start' => array(
-			 'dateTime' => $startD,
-			 'timeZone' => 'America/New_York',
-		  ),
-		  'end' => array(
-			 'dateTime' => $EndD,
-			 'timeZone' => 'America/New_York',
-		  ),
-		  'reminders' => array(
-				'useDefault' => FALSE,
-				'overrides' => array(
-						array('method' => 'email', 'minutes' => 24 * 60),
-			 ),
-		  ),
-	));
-	
-	#print_r($event);
-	#echo </br>;
-	#var_dump($event);
-	
-	return $event;
-	$calendarId = 'primary';
-	#$event = $service->events->insert($calendarId, $event);
-	#printf('Event created: %s\n', $event->htmlLink);
+	function getEventByID($ID){
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, "https://web.njit.edu/~jsr24/CS490/getEventByID.php"); 
+		curl_setopt($ch, CURLOPT_POSTFIELDS, "ID=".$ID);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		$getEventReply = curl_exec($ch);
+		curl_close($ch);
+		#echo "<br/>";
+		#$value = (json_decode($getEventReply,true));
+		return $getEventReply;	#review reply from DB	
 	}
+	
+	
+	function addGoogleCalendarEvent($title, $Place, $description, $startD, $EndD, $linkSite){
+		$event = new Google_Service_Calendar_Event(
+			array(
+			  'summary' 		=> $title,
+			  'location' 		=> $Place,
+			  'description' 	=> $description,
+			  #'htmlLink'		=> $linkSite,
+			  'start' => array(
+				 'dateTime' => $startD,
+				 'timeZone' => 'America/New_York',
+			  ),
+			  'end' => array(
+				 'dateTime' => $EndD,
+				 'timeZone' => 'America/New_York',
+			  ),
+			  'reminders' => array(
+					'useDefault' => FALSE,
+					'overrides' => array(
+							array('method' => 'email', 'minutes' => 24 * 60),
+				 ),
+			  ),
+		));
+		
+		#print_r($event);
+		#echo </br>;
+		#var_dump($event);
+		
+		#return $event;
+		$calendarId = 'primary';
+		$event = $service->events->insert($calendarId, $event);
+		printf('Event created: %s\n', $event->htmlLink);
+		}
 	
 	
 	
@@ -50,6 +66,14 @@ function addGoogleCalendarEvent($title, $Place, $description, $startD, $EndD, $l
 	$startTime   	=	$_POST['startTime'];
 	$endTime     	=	$_POST['endTime'];
 	$eventID			=	$_POST['eventID'];
+	
+	
+	#if(isset($eventID)){
+	#	$event = getEventByID($eventID);
+	#}
+	
+	
+	
 	
 	#Format Start Date and End Date with GoogleAPI specifications 
 	#Sample: "2015-05-28T17:00:00Z"
@@ -68,13 +92,13 @@ function addGoogleCalendarEvent($title, $Place, $description, $startD, $EndD, $l
 	#      var form = '<form name="'+frmName+'" method="post" action="'+url'">'+pe+'</form>';
 	
 	
-	echo $linkSite;
+	#echo $linkSite;
 	
 	
 	
 	#call function
-	$result = addGoogleCalendarEvent($title, $Place, $description, $startD, $EndD, $linkSite);
-	print_r( $result);
+	$result = addGoogleCalendarEvent($title, $Place, $description, $startD, $endD, $linkSite);
+	var_dump( $result);
 	
 	
 	
--- quickstart.php
+++ quickstart.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 
 define('APPLICATION_NAME', 'Google Calendar API PHP Quickstart');
-define('CREDENTIALS_PATH', '~/.credentials/calendar-php-quickstart.json');
+define('CREDENTIALS_PATH', '~/.credentials/calendar-php-CS490.json');
 define('CLIENT_SECRET_PATH', __DIR__ . '/client_secret.json');
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/calendar-php-quickstart.json
@@ -97,3 +97,6 @@ if (count($results->getItems()) == 0) {
   }
 }
 
+echo CREDENTIALS_PATH;
+echo "<\br>";
+echo CLIENT_SECRET_PATH;
