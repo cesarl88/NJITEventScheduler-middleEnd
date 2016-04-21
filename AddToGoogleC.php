09d0cf69e3704d92ffd909ca09b731630495b838
sabes231 <sabes231@yahoo.com>
2016-04-20 23:09:55 -0400

    GCAl

--- AddToGoogleC.php
+++ AddToGoogleC.php
@@ -94,11 +94,11 @@
 	if (isset($_GET['code'])) {
 		$client->authenticate($_GET['code']);
 		$_SESSION['access_token'] = $client->getAccessToken();
-		$redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
-		header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
-		
-		#$redirect = 'https://web.njit.edu/~jsr24/CS490/val.php';
+		#$redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
 		#header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
+		
+		$redirect = 'https://web.njit.edu/~jsr24/CS490/val.php';
+		header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
 	}
 	
 	if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
