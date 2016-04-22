4787945a5557f47cc004846180a5d7f5713d0b44
sabes231 <sabes231@yahoo.com>
2016-04-21 22:36:20 -0400

    test DateFormat

--- formatDate.php
+++ formatDate.php
@@ -1,26 +1,25 @@
 <?php
 #ini_set('display_errors', 'On');
 #error_reporting(E_ALL);	
-
-
-#function formatDate($Date, $Time){
 	
-function formatDate(){
 	
-	$val = $Date."T".$Time."Z";
-	#$val = "2016-04-21T12:00:00Z";
+	function formatDate($Date, $Time){
+		
+		
+		$val = $Date."T".$Time."Z";
+		#$val = "2016-04-21T12:00:00Z";
+		
+		$d2 = new DateTime($val);
+		#var_dump($d2);
+		
+		return $d2; 
+		
+	}	
 	
-	$d2 = new DateTime($val);
-	#var_dump($d2);
-	
-	return $d2; 
-	
-}	
-
-$Date = $_POST['Date'];
-$Time = $_POST['Time'];
-#
-$Val = formatDate($Date, $Time);
-echo $Val;
+	$Date = $_POST['Date'];
+	$Time = $_POST['Time'];
+	#
+	$Val = formatDate($Date, $Time);
+	echo $Val;
 
 ?>
