a53f88f6957c047a376bd719894517a544e11f1c
sabes231 <sabes231@yahoo.com>
2016-04-22 00:07:01 -0400

    aGC

--- AddToGoogleC.php
+++ AddToGoogleC.php
@@ -3,8 +3,8 @@
 	session_start();
 	date_default_timezone_set('America/New_York');
 
-	ini_set('display_errors', 'On');
-	error_reporting(E_ALL);	
+	#ini_set('display_errors', 'On');
+	#error_reporting(E_ALL);	
 
 	
 	#print_r(get_defined_vars());
@@ -29,7 +29,6 @@
 	define('CLIENT_SECRET_PATH', __DIR__ . '/client_secret.json');
 	define('SCOPES', implode(' ', array(Google_Service_Calendar::CALENDAR)));
 	define('REDIRECT_URI', 'https://web.njit.edu/~jsr24/CS490/AddToGoogleC.php');
-	#define('REDIRECT_URI', 'urn:ietf:wg:oauth:2.0:oob');
 	define('CLIENT_ID', '730791246182-sjnjd2b3sf6d3mhemda24eukho3jtien.apps.googleusercontent.com');
 
 	//This function is supposed to get all event details
@@ -46,37 +45,6 @@
 	}
 	
 	
-	#function authenticate(){
-	#	$ch = curl_init();
-	#	curl_setopt($ch, CURLOPT_URL, "https://accounts.google.com/o/oauth2/v2/auth"); 
-	#	curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array(
-	#																					"scope"				=> "https://www.googleapis.com/auth/calendar",
-	#																					"redirect_uri" 	=> "urn:ietf:wg:oauth:2.0:oob:auto",
-	#																					"response_type"	=> "code",
-	#																					"client_id"			=> CLIENT_ID,
-	#																			)));
-	#	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-	#	$getEventReply = curl_exec($ch);
-	#	curl_close($ch);
-	#	#echo "<br/>";
-	#	#$value = (json_decode($getEventReply,true));
-	#	#return $getEventReply;	#review reply from DB	
-	#	var_dump($getEventReply);
-	#}
-	
-	
-	
-	
-	
-#	//Post variables
-#	#$title			=	$_POST['title'];
-#	#$Place			=	$_POST['Place'];
-#	#$description	=	$_POST['description'];
-#	#$startDate   	=	$_POST['startDate'];
-#	#$EndDate     	=	$_POST['EndDate'];
-#	#$startTime   	=	$_POST['startTime'];
-#	#$endTime     	=	$_POST['endTime'];
-#	
 	if(isset($_POST['ID'])){
 		$var = $_POST['ID'];
 		#var_dump ($var);
@@ -109,27 +77,14 @@
 		
 	if (isset($_GET['code'])) {
 		$client->authenticate($_GET['code']);
-		#echo "1_This executes";
 		$_SESSION['access_token'] = $client->getAccessToken();
-		#echo "2_This executes";
-		
 		$redirect = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
-		#echo "3_This executes";
-		
-	#	header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
-	#	echo "4_This executes";
-	#	
-	#	#$redirect = 'https://web.njit.edu/~jsr24/CS490/val.php';
-	#	#header('Location: ' . filter_var($redirect, FILTER_SANITIZE_URL));
 	}
 	
-	#if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
 	if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
-		#echo "AccessTokenExecutes</br>";
 		$client->setAccessToken($_SESSION['access_token']);
 	} 
 	else {
-		#echo "AccessTokenExecutes</br>";
 		$authUrl = $client->createAuthUrl();
 	}
 	
@@ -144,63 +99,32 @@
 		
 		#call Function to get Event details
 		$eventDetails = getEventByID($eventID);
-		#var_dump($eventDetails);
 		
 		$eventArray = json_decode($eventDetails,true);
 		$eventArray = $eventArray['Event'];
-		#var_dump($eventArray);
-		
-		
 		
 		$title       		= 		$eventArray['Title'];
 		$Place       		= 		$eventArray['Place'];
-		$description      = 		'Event Name:'."\n".$eventArray['EventName']."\n".
-										'Description:'."\n".$eventArray['Description']."\n".
-										'link'."\n".$eventArray['link']."\n".
-										'Organization'."\n".$eventArray['Organization']."\n".
-										'Submitter'."\n".$eventArray['Submitter'];
+		$description      = 		'Event Name:'."\n\t".$eventArray['EventName']."\n".
+										'Description:'."\n\t".$eventArray['Description']."\n".
+										'link:'."\n\t".$eventArray['link']."\n".
+										'Organization:'."\n\t".$eventArray['Organization']."\n".
+										'Submitter:'."\n\t".$eventArray['Submitter'];
 		$startDate 			= 		$eventArray['startDate'];
 		$startTime			= 		$eventArray['startTime'];
 		$EndDate       	= 		$eventArray['EndDate'];
 		$endTime       	= 		$eventArray['endTime'];
 		$addToGoogle		= 		$eventArray['addToGoogle'];
 		
-		#var_dump($startDate);
-		#var_dump($startTime);
-		#var_dump($EndDate);  
-		#var_dump($endTime);  
 		
-		
-		
-	
+		#Create date objects
 		$val = $startDate."T".$startTime."Z";
 		$startD = new DateTime($val);
-		#$startD = date_format($startD, 'Y-m-d\TH:i:s');
-		#$startD2 = new DateTime($startD);
 		
-		#var_dump(date_format($startD, "Y-m-d\TH:i:sP" ));
-		echo "</br>";
-		echo "</br>";
-		#var_dump($startD2);
-		echo "</br>";
-		echo "</br>";
-		#echo date_format($startD, 'Y-m-d\TH:i:sP');
 				
 		$val2 = $EndDate."T".$endTime."Z";
 		$endD = new DateTime($val2);
-		#echo date_format($endD, "Y-m-d\TH:i:s");
 		
-		#date_format($date, 'Y-m-d H:i:s');
-		
-		#var_dump($endD);
-		echo "</br>";
-		#echo date_format($endD, 'Y-m-d\TH:i:sP');
-		
-		
-		
-		#calll function to format Dates
-		#$startD 	=	formatDate($startDate,$startTime);
-		#$endD		=	formatDate($EndDate, $endTime);
 		
 		#echo "</br>";
 		#var_dump($startD);
@@ -216,12 +140,10 @@
 			  #'end'				=>	$endD,
 			  
 			   'start' => array(
-				 #'dateTime' => $startD,
 				 'dateTime' => date_format($startD, "Y-m-d\TH:i:s"),
 				 'timeZone' => "America/New_York",
 			  ),
 			  'end' => array(
-				 #'dateTime' => $endD,
 				 'dateTime' => date_format($endD, "Y-m-d\TH:i:s"),
 				 'timeZone' => "America/New_York",
 			  ),
