aa0bbc7b00b79cc05f58cc7ebdfaefd08984836f
sabes231 <sabes231@yahoo.com>
2016-04-21 22:16:22 -0400

    test DateFormat

--- formatDate.php
+++ formatDate.php
@@ -5,14 +5,21 @@
 
 function formatDate($Date, $Time){
 	$val = $Date."T".$Time."Z";
+	
+	$d = new DateTime($val);
+	echo $d->format('Y-m-d\TH:i:s'); // 2011-01-01T15:03:01.012345
+	
+	
+	
 	return $val; 
+	
 	#echo date();
 }	
 
-#$Date = $_POST['Date'];
-#$Time = $_POST['Time'];
+$Date = $_POST['Date'];
+$Time = $_POST['Time'];
 #
-#$Val = formatDate($Date, $Time);
-#echo $Val;
+$Val = formatDate($Date, $Time);
+echo $Val;
 
 ?>
